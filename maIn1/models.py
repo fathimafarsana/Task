b12c27ee173107deac9bff3e2438d09e9c7a3671
fathimafarsana <115332813+fathimafarsana@users.noreply.github.com>
2023-05-25 13:24:36 +0530

    Update models.py

--- maIn1/models.py
+++ maIn1/models.py
@@ -4,7 +4,7 @@ class AnnualData(models.Model):
     field1 = models.IntegerField()
     field2 = models.IntegerField()
     field3 = models.IntegerField()
-    # Add more fields as needed
+    
 
     def __str__(self):
         return self.field1
